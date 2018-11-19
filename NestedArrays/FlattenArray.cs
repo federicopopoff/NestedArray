59277f19acdb6966ec54c122c426ccb5fba4fe87
federicopopoff <popofffederico@gmail.com>
2018-11-19 19:55:28 -0300

    Nested Array Solution

---
+++ NestedArrays/FlattenArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CitrusByte
+{
+    public static class Exercise
+    {
+        /// <summary>
+        /// Given an array of arbitrarily nested arrays of integers, convert into a flat array of integers. 
+        /// e.g. [[1,2,[3]],4] → [1,2,3,4].
+        /// </summary>
+        public static int[] FlattenArray(ArrayList toFlatten)
+        {
+            List<int> result = new List<int>();
+            try {
+                Recursive(toFlatten, ref result);
+            }
+            catch {
+                result.Clear();
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Private method to flatten array. 
+        /// </summary>
+        private static void Recursive(ArrayList toFlatten, ref List<int> result)
+        {
+            if (result == null)
+                result = new List<int>();
+
+            for (int i = 0; i < toFlatten.Count; ++i)
+            {
+                if (toFlatten[i] is ArrayList) {
+                    Recursive(toFlatten[i] as ArrayList, ref result);
+                }
+                else if (toFlatten[i] is int) {
+                    result.Add((int)toFlatten[i]);
+                }
+                else {
+                    throw new Exception("Expected an array of arrays or integers");
+                }
+            }
+        }
+    }
+}
---
+++ NestedArraysTests/ExerciseTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace CitrusByte.Tests
+{
+    [TestClass()]
+    public class ExerciseTests
+    {
+        [TestMethod()]
+        public void SampleTest()
+        {
+            ArrayList sampleTest = new ArrayList() { new ArrayList() { 1, 2, new ArrayList() { 3 } }, 4 };
+            int[] expectedResult = new int[] { 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expectedResult, Exercise.FlattenArray(sampleTest));
+        }
+
+        [TestMethod()]
+        public void SimpleTest()
+        {
+            ArrayList simpleTest = new ArrayList() { 5, 4, 3, 2, 1 };
+            int[] expectedResult = new int[] { 5, 4, 3, 2, 1 };
+            CollectionAssert.AreEqual(expectedResult, Exercise.FlattenArray(simpleTest));
+        }
+
+        [TestMethod()]
+        public void EmptyArray()
+        {
+            int[] emptyArray = new int[0];
+            CollectionAssert.AreEqual(emptyArray, Exercise.FlattenArray(null), "Null List");
+            CollectionAssert.AreEqual(emptyArray, Exercise.FlattenArray(new ArrayList()), "Empty List");
+        }
+    }
+}
